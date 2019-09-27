ae3d24f31a50c46639eb267928ec65fcec1b1d97
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-27 13:46:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-function fetchBooks() {
-  
-  return fetch('https://anapioficeandfire.com/api/books').then(function(response) {
-  
 
-  return response.json();
-}).then (function(json){
-  
-console.log(json)
-renderBooks(json)
-});
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      renderBooks(json);
+    });
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector("main");
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   fetchBooks();
-
-})
-
-  
+});
